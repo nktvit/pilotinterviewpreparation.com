9c959505d1c0ce9b19efedd8b5b47ab72426779e
nktvit <nikita.vitkovsky@icloud.com>
2025-01-10 12:07:37 +0000

    typo fix

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -360,7 +360,7 @@
             </div>
             <div class="relative z-10 h-full flex flex-col justify-center items-center px-4 md:px-6 lg:px-8">
                 <h3 class="text-3xl md:text-4xl lg:text-5xl text-white text-center font-bold mb-8 md:mb-16">
-                    Trainings That<br/>Get Results
+                    Training That<br/>Gets Results
                 </h3>
                 <div class="max-w-xs lg:max-w-lg">
                     <p class="text-xl md:text-xl lg:text-2xl text-primary-foreground text-center font-bold">
