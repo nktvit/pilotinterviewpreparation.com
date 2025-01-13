a7b2d96f5717e736bf226dab72d66d6f0b2f300a
nktvit <nikita.vitkovsky@icloud.com>
2025-01-13 19:41:36 +0000

    deploy settings removed base path

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,9 @@
 /** @type {import('@sveltejs/kit').Config} */
 import adapter from '@sveltejs/adapter-static';
-const dev = process.env.NODE_ENV === 'development';
 
 export default {
 	kit: {
 		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
 			pages: 'build',
 			assets: 'build',
 			fallback: 'index.html',
@@ -15,9 +12,6 @@ export default {
 			prerender: {
 				default: true
 			}
-		}),
-		paths: {
-			base: dev ? '' : process.env.BASE_PATH,
-		}
+		})
 	}
 };
